c7506d29f45206b6645fa9c1d1dd652f9f10a9a4
Amanda <amporto10@gmail.com>
2021-04-17 19:35:21 -0300

    Entidades pai e filhas redefinidas

--- eVacinaConn.session.sql
+++ eVacinaConn.session.sql
@@ -4,43 +4,67 @@ CREATE TABLE cidadao(
     nome VARCHAR(50) NOT NULL,
     municipio VARCHAR(30) NOT NULL,
     data_nasc DATE NOT NULL,
+    cod_contato VARCHAR(6) NOT NULL,
+    cod_historico VARCHAR(6) NOT NULL,
 
-    PRIMARY KEY (cpf)
+    PRIMARY KEY (cpf),
+
+    CONSTRAINT FK_CidadaoContato 
+    FOREIGN KEY (cod_contato) 
+    REFERENCES contato(cod_contato)
+    ON DELETE CASCADE,
+
+    CONSTRAINT FK_CidadaoHistorico 
+    FOREIGN KEY (cod_historico) 
+    REFERENCES historico_vac(cod_historico)
 );
 
--- @block 
+
 CREATE TABLE contato(
-    cpf VARCHAR(11) NOT NULL UNIQUE,
+    cod_contato VARCHAR(6),
     tel_celular VARCHAR(11) NOT NULL UNIQUE,
     tel_fixo VARCHAR(10),
     email VARCHAR(35),
 
-    PRIMARY KEY (cpf, tel_celular)
+    PRIMARY KEY (cod_contato)
 );
 
--- @block 
+
 CREATE TABLE historico_vac(
-    cpf_cidadao VARCHAR(11) NOT NULL,
+    cod_historico VARCHAR(6),
     cod_frasco VARCHAR(6) NOT NULL,
     cod_func VARCHAR(6) NOT NULL,
     prim_dose BOOLEAN NOT NULL,
     seg_dose BOOLEAN,
-    data_aplicacao DATE,
+    data_aplicacao DATE NOT NULL,
 
+    PRIMARY KEY (cod_historico), 
 
-    PRIMARY KEY (cpf_cidadao, cod_frasco)
+    CONSTRAINT FK_HistFrasco
+    FOREIGN KEY (cod_frasco)
+    REFERENCES frasco(cod_frasco)
+    ON DELETE RESTRICT,
+
+    CONSTRAINT FK_HistFunc
+    FOREIGN KEY (cod_func)
+    REFERENCES funcionario(cod_func)
+    
 );
 
--- @block 
+
 CREATE TABLE frasco(
     cod_frasco VARCHAR(8),
     cod_vacina VARCHAR(6),
     lote VARCHAR(4) NOT NULL,
 
-    PRIMARY KEY (cod_frasco)
+    PRIMARY KEY (cod_frasco),
+
+    CONSTRAINT FK_FrascoVac
+    FOREIGN KEY (cod_vacina)
+    REFERENCES vacina(cod_vacina)
 );
 
--- @BLOCK
+
 CREATE TABLE vacina(
     cod_vacina VARCHAR(6),
     nome VARCHAR(20) NOT NULL,
@@ -50,76 +74,45 @@ CREATE TABLE vacina(
     PRIMARY KEY (cod_vacina)
 );
 
--- @BLOCK
+
 CREATE TABLE armazena(
     cod_posto VARCHAR(6),
     cod_vacina VARCHAR(6),
     estoque BIGINT NOT NULL,
 
-    PRIMARY KEY (cod_posto, cod_vacina)
+    PRIMARY KEY (cod_posto, cod_vacina),
+
+    CONSTRAINT FK_ArmazenaVac
+    FOREIGN KEY (cod_vacina)
+    REFERENCES vacina(cod_vacina)
+
+    CONSTRAINT FK_ArmazenaPosto
+    FOREIGN KEY (cod_posto)
+    REFERENCES posto(cod_posto)
+
 );
 
--- @BLOCK
+
 CREATE TABLE posto(
     cod_posto VARCHAR(6),
     nome VARCHAR(40) NOT NULL,
     municipio VARCHAR(30) NOT NULL,
 
     PRIMARY KEY (cod_posto)
+
 );
 
--- @BLOCK
 CREATE TABLE funcionario(
     cod_func VARCHAR(6),
     nome VARCHAR(50) NOT NULL,
-    -- cod_posto do funcionario pode ser
-    -- null pela regra de negocio 
-    cod_posto VARCHAR(6) NOT NULL,
+    cod_posto VARCHAR(6),
 
-    PRIMARY KEY (cod_func)
+    PRIMARY KEY (cod_func),
+
+    CONSTRAINT FK_FuncPosto
+    FOREIGN KEY (cod_posto)
+    REFERENCES posto(cod_posto)
+    ON DELETE SET NULL
+    
 );
-
--- Foreign keys do plastino
-
--- @BLOCK
-ALTER TABLE cidadao
-ADD CONSTRAINT FK_CidadaoContato
-FOREIGN KEY (cpf) REFERENCES Contato(cpf)
-ON DELETE CASCADE;
-
--- @BLOCK
-ALTER TABLE cidadao
-DROP FOREIGN KEY FK_CidadaoContato;
-
--- @BLOCK
-ALTER TABLE historico_vac
-ADD CONSTRAINT FK_HistVacFrasco
-FOREIGN KEY (cod_frasco) REFERENCES frasco(cod_frasco)
-ON DELETE RESTRICT;
-
--- @BLOCK
-ALTER TABLE funcionario
-MODIFY COLUMN cod_posto VARCHAR(6);
-
--- @BLOCK
-ALTER TABLE funcionario
-ADD CONSTRAINT FK_FuncionarioPosto
-FOREIGN KEY (cod_posto) REFERENCES posto(cod_posto)
-ON DELETE SET NULL;
-
--- Outras foreign keys 
--- @BLOCK
-ALTER TABLE historico_vac
-ADD CONSTRAINT FK_CidadaoHistorico
-FOREIGN KEY (cpf_cidadao) REFERENCES cidadao(cpf)
-
--- @BLOCK
-ALTER TABLE armazena
-ADD CONSTRAINT FK_ArmazenaVacina
-FOREIGN KEY (cod_vacina) REFERENCES vacina(cod_vacina)
-
--- @BLOCK
-ALTER TABLE armazena
-ADD CONSTRAINT FK_ArmazenaPosto
-FOREIGN KEY (cod_posto) REFERENCES posto(cod_posto)
 
